d6df9847e8442cfa9eb82efd3e35e32075386269
olvrdu44 <olivier@rainetcom.fr>
2018-05-22 15:43:43 +0200

    v221543

--- www/js/index.js
+++ www/js/index.js
@@ -19,27 +19,7 @@
 var app = {
     // Application Constructor
     initialize: function() {
-		alert("ok");
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
-		var onSuccess = function(position) {
-			alert('Latitude: '          + position.coords.latitude          + '\n' +
-				  'Longitude: '         + position.coords.longitude         + '\n' +
-				  'Altitude: '          + position.coords.altitude          + '\n' +
-				  'Accuracy: '          + position.coords.accuracy          + '\n' +
-				  'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
-				  'Heading: '           + position.coords.heading           + '\n' +
-				  'Speed: '             + position.coords.speed             + '\n' +
-				  'Timestamp: '         + position.timestamp                + '\n');
-		};
-
-		// onError Callback receives a PositionError object
-		//
-		function onError(error) {
-			alert('code: '    + error.code    + '\n' +
-				  'message: ' + error.message + '\n');
-		}
-
-		navigator.geolocation.getCurrentPosition(onSuccess, onError);
     },
 
     // deviceready Event Handler
@@ -48,8 +28,6 @@ var app = {
     // 'pause', 'resume', etc.
     onDeviceReady: function() {
         this.receivedEvent('deviceready');
-		
-			alert("oups");
     },
 
     // Update DOM on a Received Event
@@ -62,9 +40,27 @@ var app = {
         receivedElement.setAttribute('style', 'display:block;');
 
         console.log('Received Event: ' + id);
-		
-		alert("la");
+
+		navigator.geolocation.getCurrentPosition(onSuccess, onError);
     }
 };
 
 app.initialize();
+
+var onSuccess = function(position) {
+	alert('Latitude: '          + position.coords.latitude          + '\n' +
+		  'Longitude: '         + position.coords.longitude         + '\n' +
+		  'Altitude: '          + position.coords.altitude          + '\n' +
+		  'Accuracy: '          + position.coords.accuracy          + '\n' +
+		  'Altitude Accuracy: ' + position.coords.altitudeAccuracy  + '\n' +
+		  'Heading: '           + position.coords.heading           + '\n' +
+		  'Speed: '             + position.coords.speed             + '\n' +
+		  'Timestamp: '         + position.timestamp                + '\n');
+};
+
+// onError Callback receives a PositionError object
+//
+function onError(error) {
+	alert('code: '    + error.code    + '\n' +
+		  'message: ' + error.message + '\n');
+}
